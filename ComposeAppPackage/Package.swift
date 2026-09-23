cf208954016e8ad7455319089e588fe97f2d627a
Guillermo Martin <guillermo.martin@avenga.com>
2026-09-23 11:38:34 -0300

    feat: consume composeApp XCFramework from a remote GitHub Release
    Switch ComposeAppPackage's binaryTarget from a local path to the
    v0.1.0 GitHub Release asset (url + checksum), and disable the now-
    redundant Xcode Run Script that reassembled the local Debug XCFramework.

--- ComposeAppPackage/Package.swift
+++ ComposeAppPackage/Package.swift
@@ -10,7 +10,9 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ComposeApp",
-            path: "../composeApp/build/XCFrameworks/debug/ComposeApp.xcframework"
+//             path: "../composeApp/build/XCFrameworks/debug/ComposeApp.xcframework"
+            url: "https://github.com/guillermomartinavenga-code/rick-and-morty-kmp/releases/download/v0.1.0/ComposeApp.xcframework.zip",
+            checksum: "280f7c0d3e51383b1a0eab7bfbb5defda29c9907faf8b0e85c1b51ab06a266cb"
         )
     ]
 )
